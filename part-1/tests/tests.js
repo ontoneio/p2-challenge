c78e7b0c2f78829756ceaed256ebb94bfceae770
ontoneio <ontoneio.mm@gmail.com>
2017-07-24 23:00:51 -0700

    part 1 first commit

---
+++ part-1/tests/tests.js
@@ -0,0 +1,34 @@
+const funk = require('../functions');
+const expect = require('chai').expect;
+
+
+describe("Phase 2 part-1 functions", () => {
+    
+    describe("month()", (params) => {
+        it("", () => {
+
+            expect()
+        })
+    })
+
+    describe("reverseSentence()", (params) => {
+        it("", () => {
+            
+            expect()
+        })
+    })
+
+    describe("propNames()", (params) => {
+        it("", () => {
+            
+            expect()
+        })
+    })
+
+    describe("filterBetween()", (params) => {
+        it("", () => {
+            
+            expect()
+        })
+    })
+} )
